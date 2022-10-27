6279705d6aedd304d16f45b722751a840d5da8a9
Dan Hope <dplhope@gmail.com>
2022-10-27 13:12:35 +0100

    initial form setup

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -4,7 +4,21 @@
 
 <div class="bg-white p-4 w-2/3 mx-auto">
     <h2 class="text-center text-2xl">Upload your dog!</h2>
-    <p>Please add your form code here</p>
+    <form 
+        action="PictureController.php" 
+        method="post" 
+        enctype="multipart/form-data">
+            <input
+                type="file"
+                name="image"
+                > 
+            <input
+                type="text"
+                name="name"
+                placeholder="e.g. Rex"
+                >
+    </form>
+
 </div>
 
 @endsection
