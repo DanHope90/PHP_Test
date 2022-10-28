e1128d5c09eed2866a5aac557f3f2cda24fae719
Dan Hope <dplhope@gmail.com>
2022-10-28 10:01:19 +0100

    form action updated

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
 <div class="bg-white p-4 w-2/3 mx-auto">
+    <body>
     <h2 class="text-center text-2xl">Upload your dog!</h2>
     <form 
-        action="PictureController.php" 
+        action="/pictures/create" 
         method="post" 
         enctype="multipart/form-data">
             <input
@@ -21,7 +22,7 @@
                 Submit
             </button>
     </form>
-
+    </body>
 </div>
 
 @endsection
