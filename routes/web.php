505ee24c41d0ef4250d5195b7ee74b7ca2f7d36b
Dan Hope <dplhope@gmail.com>
2022-11-01 13:08:48 +0000

    post route updated

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::resources([
 Route::post('/pictures/{picture}/upvote', [PictureController::class, 'upvote'])->name('pictures.upvote');
 
 // post picture to database and local storage 
-Route::post('/pictures/create', [PictureController::class, 'store']);
+Route::post('/pictures/store', [PictureController::class, 'store']);
