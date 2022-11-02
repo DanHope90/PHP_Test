f5d4fa3089264baaed3fabfc12ffbc7669657968
Dan Hope <dplhope@gmail.com>
2022-11-02 11:04:16 +0000

    upvote tests added

--- tests/Feature/PictureControllerTest.php
+++ tests/Feature/PictureControllerTest.php
@@ -62,8 +62,29 @@ class PictureControllerTest extends TestCase
         $this->assertFileExists(storage_path('app/public/' . $file->hashName()));
     }
 
-    // public function test_upvote_a_dog()
-    // {
+    public function test_upvote_a_dog()
+    {
+        // response increment a pictures votes in tables votes column 
 
-    // }
+        //saves to database 
+
+        $dog = Picture::create([
+            'name' => 'test name',
+            'file_path' => 'dog.jpg',
+        ]);
+        $dog->id = 1;
+        $dog->votes = 10;
+        $dog->save();
+
+        $response = $this->post('/pictures/1/upvote', [
+            'picture' => $dog
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+        
+        $this->assertDatabaseHas('pictures', [
+            'id' => 1,
+            'votes' => 11
+        ]);
+    }
 }
